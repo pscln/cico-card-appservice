74cc1f3d2353333bc5b732f6b0dda0af8a2be221
pascal <psn754@gmail.com>
2022-03-15 20:42:37 +0100

    add /status & resolve port from azure properties

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/', (req, res) => {
+app.get('/status', (req, res) => {
   res.send('{"service_status": "running"}]')
 })
 
