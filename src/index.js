b233d90cd2e5c4f15d32fc58d549446c21c8e3c9
pascal <psn754@gmail.com>
2022-03-15 17:28:28 +0100

    trigger cd pipeline in azure

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/', (req, res) => {
+  res.send('{"service_status": "running"}]')
+})
+
 app.listen(port, () => {
   console.log(`CICO example for Azure App Service listening on port ${port}`)
 })
